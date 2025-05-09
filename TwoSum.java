db3a0cbe961ff697906f3b62b8d767bcbd6edde5
Wuonam <odeedward04@gmail.com>
2025-05-10 00:20:14 +0200

    Complexity reduce

--- TwoSum.java
+++ TwoSum.java
@@ -1,4 +1,5 @@
 import java.util.Arrays;
+import java.util.HashMap;
 
 public class TwoSum {
 
@@ -17,10 +18,35 @@ public class TwoSum {
     }
 
 
+    public static int[] twoSum2(int[] nums, int target){
+
+        //I want to improve performance by introducing memoisation
+
+        //Hashmap wither Integer for both key and value
+        //I will store the compliment otf the number I am at and  it's actual index
+        //[2,3,5]  target 5
+        //will store 3 and 0 incase of two and iterate to find 3
+
+
+
+        HashMap<Integer, Integer> checker = new HashMap<>();
+
+        for(int i = 0; i < nums.length; i++){
+            if(checker.containsKey(nums[i])){
+                return new int[]{ checker.get(nums[i]),i};
+            }
+            checker.put(target- nums[i], i);
+        }
+        return null;
+
+    }
+
+
     public static void main(String[] args) {
         int i = 6;
         int []arr= {3,3};
         System.out.println(Arrays.toString(twoSum(arr, i)));
+        System.out.println(Arrays.toString(twoSum2(arr, i)));
     }
 }
 
