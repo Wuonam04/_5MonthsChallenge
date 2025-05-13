a5c0f1a43e5b6b1c7d9fd056ae22e44444f66838
Wuonam <odeedward04@gmail.com>
2025-05-13 18:55:59 +0200

    BuyStock with Memoization.Code is optimized

--- BuyStock.java
+++ BuyStock.java
@@ -1,3 +1,6 @@
+import java.util.HashMap;
+import java.util.Map;
+
 public class BuyStock {
 
     /*You are given an array prices where prices[i] is the price of a given stock on the ith day.
@@ -28,11 +31,30 @@ public class BuyStock {
         return profit;
 
     }
+    public static int maxProfit2(int[] prices) {
+
+        int n = prices.length;
+        int profit = 0;
+
+        Map<Integer, Integer> checker = new HashMap<>();
+        //Method is too slow
+        for(int i = 0; i < n-1; i++){
+                if(prices[i+1] - prices[i] > profit){
+                    profit = prices[i+1] - prices[i];
+                    checker.put(i,profit);      //passing my profit values and corresponding profits
+
+                }
+
+        }
+        System.out.println(checker.values());
+        return profit;
+
+    }
 
     public static void main(String[] args) {
 
-        int [] a = {67,9};
-        System.out.println(maxProfit(a));
+        int [] a = {97,78};
+        System.out.println(maxProfit2(a));
 
     }
 
