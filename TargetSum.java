f755728105863df449383a3eb98125bbd03ca7d4
Wuonam <odeedward04@gmail.com>
2025-05-19 18:13:29 +0200

    update

--- TargetSum.java
+++ TargetSum.java
@@ -5,12 +5,13 @@ public class TargetSum {
 
     public static int targetSum(int [] nums, int target){
 
-        return recu(nums, nums.length - 1, target);
+        return recu(nums, nums.length - 1, target);   //stating from the last index
 
     }
     public static int recu(int[] nums, int i, int target){
+        int count = 0;
         if(i< 0){
-            return target == 0 ? 1 :0;  //base case if target == 0 return 1 else 0
+            return target == 0 ? 1 :0;  //base case if target == 0 return 1 else 0 and this 1 is the count
         }
         int includelast = recu( nums, i-1, target - nums[i]);
         int subtractlast = recu(nums, i-1, target + nums[i]);
@@ -19,7 +20,7 @@ public class TargetSum {
     }
 
     public static void main(String[] args) {
-        int [] ar = {1,1,1,1,1};
+        int [] ar = {1,1,1,1};
 
         System.out.println(targetSum(ar, 3));
     }
